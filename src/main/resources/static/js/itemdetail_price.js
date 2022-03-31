21b9340a3af4970610d5d81b9173942c8dd1f1fc
kashimamiyu <pg.studykm@gmail.com>
2022-03-31 15:33:37 +0900

    商品詳細画面JavaScript

--- src/main/resources/static/js/itemdetail_price.js
+++ src/main/resources/static/js/itemdetail_price.js
@@ -1,7 +1,50 @@
-'use strict';
-$(function(){
+"use strict";
+
+$(function () {
+	let total = 0;
+	let total2 = 0;
+	let total3=0;
+	$('input:radio[name="size"]').change(function() {
+		if($('input:radio[name="size"]:checked').val()=="M"){
+			total=$("#priceM").val();
+		}else{
+			total=$("#priceL").val();
+		}
+		total=parseInt(total);
+		let totalAll = (total+total2)*total3;
+		$("#total-price").text(totalAll+"円");
+	}).trigger('change');
 	
 	
+	let top2 =0;
+	$('input:checkbox[name="orderToppings"]').change(function() {
+        let top = $('#topping input:checkbox:checked').length;
+        if($('input:radio[name="size"]:checked').val()=="M"){
+			if(top2>top){
+				total2 = total2 - 200;
+			}else{
+				total2 = total2 + 200;
+			}
+		}else if($('input:radio[name="size"]:checked').val()=="L"){
+			if(top2>top){
+				total2 = total2 - 300;
+			}else{
+				total2 = total2 + 300;
+			}
+		}
+		top2=top;
+		
+		let totalAll = (total+total2)*total3;
+		$("#total-price").text(totalAll+"円");
+    });
+
+    $('#quantity').change(function() {
+		total3 = $("#quantity").val();
+		
+		let totalAll = (total+total2)*total3;
+		$("#total-price").text(totalAll+"円");
+	
+	}).trigger('change');
 	
 	
 	
